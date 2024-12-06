bc56035a63fbdf531a1905eb962cc0ed01148baa
Jose Ozuna <joseozuna48@gmail.com>
2024-12-05 22:15:51 -0700

    Add function to display books inside the library array

--- scripts.js
+++ scripts.js
@@ -6,12 +6,51 @@ function Book() {
   this.author= "";
 }
 
-Book.prototype.createBook = function(title,author){
-    this.titile = title;
-    this.author = author;
-}
-
-function addBookToLibrary(book) {
+function addBookToLibrary(title,author) {
   // do stuff here
+    let book = new Book();
+    book.title = title;
+    book.author = author
     myLibrary.push(book);
 }
+
+
+function openModal(){
+  let modal = document.querySelector('dialog');
+
+  modal.showModal();
+}
+
+function displayLibrary(){
+  let container = document.querySelector('.books_container');
+
+  addBookToLibrary('quivole1',"author1");
+  addBookToLibrary('quivole2','author2');
+
+  for(let i=0; i<myLibrary.length; i++){
+    let content = document.createElement("div")
+    content.classList.add("book");
+
+    let info  = document.createElement("div")
+    info.classList.add('book-info');
+
+    let title = document.createElement("div")
+    title.classList.add('title');
+    title.innerText =`${myLibrary[i].title}`;
+
+    let author = document.createElement("div")
+    author.classList.add('author');
+    author.innerText = `${myLibrary[i].author}`;
+
+    info.appendChild(title);
+    info.appendChild(author);
+    content.appendChild(info);
+
+    container.appendChild(content)
+  }
+}
+document.addEventListener("DOMContentLoaded", (event) => {
+  displayLibrary();
+});
+
+
