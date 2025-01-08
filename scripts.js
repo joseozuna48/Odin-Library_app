a5e8ea6d0e2c375d829480fb99da077b29de8b17
Jose Ozuna <joseozuna48@gmail.com>
2025-01-07 17:46:19 -0700

    Add buttons functionality

--- scripts.js
+++ scripts.js
@@ -21,13 +21,39 @@ function openModal(){
   modal.showModal();
 }
 
-function displayLibrary(){
-  let container = document.querySelector('.books_container');
 
-  addBookToLibrary('quivole1',"author1");
-  addBookToLibrary('quivole2','author2');
+function deleteBook(event){
+  let index = event.target.dataset.index;
+
+  let books = document.querySelector(".library");
+  books.innerHTML = "";
+  
+  myLibrary.splice(index,1);
+
+  displayLibrary();
+}
+
+function setRead(event){
+  let index = event.target.dataset.index;
+  let books = document.querySelectorAll(".book-container .book");
+
+  if(books[index].classList.contains("read") ){
+   books[index] .classList.remove("read");
+   event.target.innerText = "Mark\n as read"
+  }else{
+    books[index].classList.add("read");
+    event.target.innerText = "Remove  read"
+  }
+}
+
+function displayLibrary(){
+  let library = document.querySelector('.library');
 
   for(let i=0; i<myLibrary.length; i++){
+
+    let bookContainer = document.createElement("div")
+    bookContainer.classList.add("book-container");
+
     let content = document.createElement("div")
     content.classList.add("book");
 
@@ -42,14 +68,45 @@ function displayLibrary(){
     author.classList.add('author');
     author.innerText = `${myLibrary[i].author}`;
 
+    let btnContainer = document.createElement("div");
+    btnContainer.classList.add("btns-container");
+
+    let deleteBtn = document.createElement("button");
+    deleteBtn.type = "button"
+    deleteBtn.textContent = "Delete\n Book";
+    deleteBtn.dataset.index = i;
+    deleteBtn.addEventListener("click", deleteBook);
+
+    let readBtn = document.createElement("button");
+    readBtn.type = "button"
+    readBtn.textContent = "Mark\n as read";
+    readBtn.dataset.index = i;
+    readBtn.addEventListener("click", setRead);
+    
+    let readTag = document.createElement("div");
+    readTag.classList.add("read-tag");
+    readTag.textContent = "READ";
+
+
+    btnContainer.appendChild(deleteBtn);
+    btnContainer.appendChild(readBtn);
+
     info.appendChild(title);
     info.appendChild(author);
     content.appendChild(info);
+    content.appendChild(readTag);
+    bookContainer.appendChild(content);
+    bookContainer.appendChild(btnContainer);
 
-    container.appendChild(content)
+    library.appendChild(bookContainer);
   }
 }
 document.addEventListener("DOMContentLoaded", (event) => {
+
+  addBookToLibrary('quivole1',"author1");
+  addBookToLibrary('quivole2','author2');
+  addBookToLibrary(' Farewell to arms',"By: Watson");
+  addBookToLibrary('Anthroposcene Reviewed','By: John Green');
   displayLibrary();
 });
 
